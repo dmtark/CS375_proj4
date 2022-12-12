114bcf6a36661ae6fbff6f54ea132aebf03b34aa
Milo Lani-Caputo <mlanicaputo@gmail.com>
2022-12-12 15:22:09 -0500

    Create cs375 word set

--- create_375_dict.py
+++ create_375_dict.py
@@ -1,5 +1,7 @@
 # creating 375 dictionary
 from tika import parser # pip install tika
+import os
+import re
 
 '''
 load all 375 files
@@ -23,11 +25,54 @@ ideas for data representation:
 '''
 
 
-def parse_latex_file(filename):
-    raw = parser.from_file(filename)
+
+
+def parse_latex_file(pdf_filename: str) -> set:
+    """Return a set containing all words in a pdf file."""
+    raw = parser.from_file(pdf_filename)
     raw_data = raw['content']
-    words = re.find_all(r"[a-zA-Z\'\-]+", raw_data) # splitting words based off of this pattern
+    words = re.findall(r"[a-zA-Z\'\-]+", raw_data) # splitting words based off of this pattern
         # splits on everything that is not a letter, backslash, apostrophe or dash
         # this doesn't include numbers...
         # r makes this a raw string (\w is just treated like a character)
+    # set all words to lowercase
+    words = [word.lower() for word in words]
     return set(words) # do not want duplicates...
+
+
+def pdf_file_names(directory: str) -> list:
+    """Return a list of the pdf filenames in a given directory."""
+    all_files = os.listdir(directory)
+    pdf_files = [f"{directory}/{str(filename)}" for filename in all_files if str(filename).endswith(".pdf")]
+
+    return pdf_files
+
+
+def master_set_from_pdf_list(pdf_list: list) -> set:
+    """Return a master set of all words in a list of pdf files."""
+    master_set = set()
+
+    for filename in pdf_list:
+        file_set = parse_latex_file(filename)
+        master_set.update(file_set)
+
+    return master_set
+
+
+def write_set_to_txt_file(word_set: set, filepath: str) -> None:
+    """Write a set of words to a txt file, one word per line."""
+    set_list = list(word_set)
+
+    with open(filepath, "w") as file:
+        file.write("\n".join(set_list))
+
+
+def main():
+    names = pdf_file_names("./assignments")
+    print(names)
+    master_set = master_set_from_pdf_list(names)
+    write_set_to_txt_file(master_set, "cs375_word_set.txt")
+
+
+if __name__ == "__main__":
+    main()
